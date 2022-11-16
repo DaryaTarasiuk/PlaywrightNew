42229f6539ab7cdcebda1de18e8e055763e1f75d
Darya.Tarasiuk <tarasiuk.darya@gmail.com>
2022-11-16 16:47:43 +0300

    added new file into the project element_state.js

---
+++ element_state.js
@@ -0,0 +1,21 @@
+const  {chromium} = require('playwright');
+
+(async()=>{
+    const browser = await chromium.launch();
+    const page = await browser.newPage();
+    await page.goto ('https://demoqa.com/automation-practice-form');
+    const firstName = await page.$('#firstName');
+    const sportCheck = await page.$('#hobbies-checkbox-1');
+    const submitBtn = await page.$('#submit');
+
+    console.log(await firstName.isDisabled());
+    console.log(await firstName.isEnabled());
+    console.log(await firstName.isEditable());
+    console.log(await sportCheck.isChecked());
+    console.log(await sportCheck.isVisible());
+    console.log(await submitBtn.isHidden());
+    console.log(await submitBtn.isVisible());
+
+    await browser.close();
+
+})();
