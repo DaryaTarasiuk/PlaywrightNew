76e6a8b632c3a7cfa1f983bf69c184bf66de929c
Darya.Tarasiuk <tarasiuk.darya@gmail.com>
2022-11-21 10:25:53 +0300

    adde a PageObjects into the project pom

--- checkradio.js
+++ checkradio.js
@@ -1,6 +1,6 @@
 const  {firefox} = require('playwright');
 
-(async()=>{
+(async()=>{ 
     const browser = await firefox.launch({headless: false, slowMo:500});
     const page = await browser.newPage();
     await page.goto ('https://www.w3schools.com/howto/howto_css_custom_checkbox.asp');
